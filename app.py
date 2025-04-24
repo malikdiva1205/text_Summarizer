ee723514981a0e5f70c4b56bbe6475e4d6fe8721
malikdiva1205 <166149659+malikdiva1205@users.noreply.github.com>
2025-04-24 15:54:10 +0530

    file

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ from bs4 import BeautifulSoup
 import requests
 import io
 
+try:
+    nlp = spacy.load('en_core_web_sm')
+except:
+    import os
+    os.system("python -m spacy download en_core_web_sm")
+    nlp = spacy.load('en_core_web_sm')
+
+
 # Download required resources
 nltk.download('wordnet')
 nltk.download('punkt')

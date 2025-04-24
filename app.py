cd7fd3c5deb932478da0d2430e53358d58361643
malikdiva1205 <166149659+malikdiva1205@users.noreply.github.com>
2025-04-24 15:55:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ from bs4 import BeautifulSoup
 import requests
 import io
 
-try:
-    nlp = spacy.load('en_core_web_sm')
-except:
-    import os
-    os.system("python -m spacy download en_core_web_sm")
-    nlp = spacy.load('en_core_web_sm')
+# try:
+#     nlp = spacy.load('en_core_web_sm')
+# except:
+#     import os
+#     os.system("python -m spacy download en_core_web_sm")
+#     nlp = spacy.load('en_core_web_sm')
 
 
 # Download required resources
